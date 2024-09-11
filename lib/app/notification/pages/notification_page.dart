733144f1585171f90de0538ac0c2a7ac20cb0c8f
priyanshuk.7374 <priyanshuk.7374@gmail.com>
2024-09-11 02:17:36 +0000

    8th apk build

--- lib/app/notification/pages/notification_page.dart
+++ lib/app/notification/pages/notification_page.dart
@@ -7,8 +7,10 @@ import 'package:moneycart/config/theme/app_pallete.dart';
 import 'package:moneycart/notification_handler.dart';
 
 class NotificationPage extends StatelessWidget {
-  final NotificationController _notificationController = Get.find<NotificationController>();
-  final NotificationHandler _notificationHandler = Get.find<NotificationHandler>();
+  final NotificationController _notificationController =
+      Get.find<NotificationController>();
+  final NotificationHandler _notificationHandler =
+      Get.find<NotificationHandler>();
 
   NotificationPage({super.key});
 
@@ -40,92 +42,100 @@ class NotificationPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Obx(() {
-        if (_notificationController.isLoading.value) {
-          return const Loader();
-        } else if (_notificationController.hasError.value) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  'assets/svgs/connection_error.svg',
-                  width: MediaQuery.of(context).size.width * 0.6,
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Failed to load notifications',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          );
-        } else if (_notificationController.notifications.isEmpty) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  'assets/svgs/no_message.svg',
-                  width: MediaQuery.of(context).size.width * 0.6,
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'No Notifications',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          );
-        } else {
-          return ListView.builder(
-            itemCount: _notificationController.notifications.length,
-            itemBuilder: (context, index) {
-              final notification = _notificationController.notifications[index];
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                child: Card(
-                  elevation: 1.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: ListTile(
-                    contentPadding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
-                    leading: const CircleAvatar(
-                      backgroundColor: Color.fromARGB(255, 77, 197, 117),
-                      child: Icon(
-                        Icons.notifications,
-                        color: Colors.white,
+      body: RefreshIndicator(
+          color: AppPallete.boldprimary,
+          onRefresh: _notificationController.fetchNotifications,
+          child: Obx(() {
+            if (_notificationController.isLoading.value) {
+              return const Loader();
+            } else if (_notificationController.hasError.value) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/svgs/connection_error.svg',
+                      width: MediaQuery.of(context).size.width * 0.6,
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Failed to load notifications',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    title: Text(
-                      notification.title,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(notification.body),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete),
-                      onPressed: () {
-                        _notificationController.deleteNotification(notification.id);
-                      },
-                    ),
-                    onTap: () {
-                      _notificationController.markAsRead(notification.id);
-                      _notificationHandler.handleNotificationClick(notification.toMap());
-                    },
-                  ),
+                  ],
                 ),
               );
-            },
-          );
-        }
-      }),
+            } else if (_notificationController.notifications.isEmpty) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/svgs/no_message.svg',
+                      width: MediaQuery.of(context).size.width * 0.6,
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'No Notifications',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: _notificationController.notifications.length,
+                itemBuilder: (context, index) {
+                  final notification =
+                      _notificationController.notifications[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 16.0),
+                    child: Card(
+                      elevation: 1.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 12.0, horizontal: 16.0),
+                        leading: const CircleAvatar(
+                          backgroundColor: Color.fromARGB(255, 77, 197, 117),
+                          child: Icon(
+                            Icons.notifications,
+                            color: Colors.white,
+                          ),
+                        ),
+                        title: Text(
+                          notification.title,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(notification.body),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () {
+                            _notificationController
+                                .deleteNotification(notification.id);
+                          },
+                        ),
+                        onTap: () {
+                          _notificationController.markAsRead(notification.id);
+                          _notificationHandler
+                              .handleNotificationClick(notification.toMap());
+                        },
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          })),
     );
   }
 }
